5de9ca9b474462f0ed04c03bce649d005d4cc7e8
smin <smin0626@gmail>
2025-01-09 18:09:08 +0900

    코드 수정

--- src/main/java/com/example/calculator2/App.java
+++ src/main/java/com/example/calculator2/App.java
@@ -23,32 +23,33 @@ public class App {
 
             // 계산 시작
             // 나눗셈 분모에 0이 들거가거나 연산자를 잘못입력하면 처음으로 이동
-            try {
-                // 계산 종료 후 추가 계산이나 기록, 삭제
-                System.out.println("더 계산하시겠습니까? (yes 입력 시 추가 계산,exit 입력 시 종료, record 입력 시 계산기록 출력, remove 입력 시 계산기록 삭제)");
-                String outText = sc.next();
-
-                if (outText.equals("exit")) {
-                    System.out.println("계산을 종료합니다.");
-                    break;
-                } else if (outText.equals("record")) {
-                    System.out.println("저장된 계산 기록:");
-                    for (String record : calculator.getRecord()) {
-                        System.out.println(record);
-                    }
-                    // 맨 처음 계산기록을 삭제
-                } else if (outText.equals("remove")) {
-                    calculator.removeRecord(0);
-                    System.out.println("기록 삭제 후:");
-                    for (String record : calculator.getRecord()) {
-                        System.out.println(record);
-                    }
-                } else if (outText.equals("yes")) {
-                    continue;
-                }
-            } catch (ArithmeticException | IllegalArgumentException e) {
-                System.out.println(e.getMessage());
+            int result = calculator.calculate(fristNum, operator, secondNum);
+            if (result == 0) {
+                continue;
             }
+            // 계산 종료 후 추가 계산이나 기록, 삭제
+            System.out.println("더 계산하시겠습니까? (yes 입력 시 추가 계산,exit 입력 시 종료, record 입력 시 계산기록 출력, remove 입력 시 계산기록 삭제)");
+            String outText = sc.next();
+
+            if (outText.equals("exit")) {
+                System.out.println("계산을 종료합니다.");
+                break;
+            } else if (outText.equals("record")) {
+                System.out.println("저장된 계산 기록:");
+                for (String record : calculator.getRecord()) {
+                    System.out.println(record);
+                }
+                // 맨 처음 계산기록을 삭제
+            } else if (outText.equals("remove")) {
+                calculator.getRemoveRecord(0);
+                System.out.println("기록 삭제 후:");
+                for (String record : calculator.getRecord()) {
+                    System.out.println(record);
+                }
+            } else if (outText.equals("yes")) {
+                continue;
+            }
+
         }
     }
 }
--- src/main/java/com/example/calculator2/Calculator.java
+++ src/main/java/com/example/calculator2/Calculator.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Calculator {
     // 결과 값 저장을 위한 배열 생성 및
-    private ArrayList<String> record = new ArrayList<>();
+    private final ArrayList<String> record = new ArrayList<>();
 
     // 생성자 (위에서 이미 초기화를 해 기본생성자로 작성해도 이상없음 안적어도 실행은 됨)
     public Calculator() {
@@ -16,40 +16,43 @@ public class Calculator {
     }
 
     // 계산 기록에서 맨 처음 기록을 삭제
-    public void removeRecord(int index) {
+    public void getRemoveRecord(int index) {
         if (index >= 0 && index < record.size()) {
             this.record.remove(index);
         }
     }
 
     // 계산 식
-    public int calculate(int fristNum, char operator, int secondNum) {
+    public int calculate(int firstNum, char operator, int secondNum) {
         int result = 0;
-
-        switch (operator) {
-            case ('+'):
-                result = fristNum + secondNum;
-                break;
-            case ('-'):
-                result = fristNum - secondNum;
-                break;
-            case ('*'):
-                result = fristNum * secondNum;
-                break;
-            case ('/'):
-                if (secondNum == 0) {
-                    throw new ArithmeticException("나눗셈의 분모에는 0이 들어갈 수 없습니다. 다시입력해주세요");
-                }
-                result = fristNum / secondNum;
-                break;
-            default:
-                throw new IllegalArgumentException("유효하지 않은 연산자입니다. 다시 입력해주세요.");
+        try {
+            switch (operator) {
+                case ('+'):
+                    result = firstNum + secondNum;
+                    break;
+                case ('-'):
+                    result = firstNum - secondNum;
+                    break;
+                case ('*'):
+                    result = firstNum * secondNum;
+                    break;
+                case ('/'):
+                    if (secondNum == 0) {
+                        throw new ArithmeticException("나눗셈의 분모에는 0이 들어갈 수 없습니다. 다시입력해주세요");
+                    }
+                    result = firstNum / secondNum;
+                    break;
+                default:
+                    throw new IllegalArgumentException("유효하지 않은 연산자입니다. 다시 입력해주세요.");
+            }
+        } catch (ArithmeticException | IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
         // 계산이 끝나면 출력
         System.out.println("계산 결과 =" + result);
 
         // 계산 결과를 배열에 추가
-        String calculate = fristNum + " " + operator + " " + secondNum + "= " + result;
+        String calculate = firstNum + " " + operator + " " + secondNum + " = " + result;
         record.add(calculate);
         return result;
     }
