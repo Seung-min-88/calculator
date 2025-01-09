b64fd9145288aa86c80769fd979e3e725ad14002
smin <smin0626@gmail>
2025-01-09 15:04:35 +0900

    수정

--- src/main/java/com/example/calculator2/App.java
+++ src/main/java/com/example/calculator2/App.java
@@ -3,7 +3,7 @@ package com.example.calculator2;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         // 객체 생성
         Calculator calculator = new Calculator();
@@ -23,7 +23,11 @@ public class App {
 
             // 계산 시작
             // 나눗셈 분모에 0이 들거가거나 연산자를 잘못입력하면 처음으로 이동
-            try {
+            int result = calculator.calculate(fristNum, operator, secondNum);
+            if (result == 0) {
+                continue;
+            }
+//            try {
                 // 계산 종료 후 추가 계산이나 기록, 삭제
                 System.out.println("더 계산하시겠습니까? (yes 입력 시 추가 계산,exit 입력 시 종료, record 입력 시 계산기록 출력, remove 입력 시 계산기록 삭제)");
                 String outText = sc.next();
@@ -32,13 +36,13 @@ public class App {
                     System.out.println("계산을 종료합니다.");
                     break;
                 } else if (outText.equals("record")) {
-                    System.out.println("저장된 계산 기록:");
+                    System.out.println("저장된 계산 기록:" + calculator.getRecord());
                     for (String record : calculator.getRecord()) {
                         System.out.println(record);
                     }
                     // 맨 처음 계산기록을 삭제
                 } else if (outText.equals("remove")) {
-                    calculator.removeRecord(0);
+                    calculator.getRemoveRecord(0);
                     System.out.println("기록 삭제 후:");
                     for (String record : calculator.getRecord()) {
                         System.out.println(record);
@@ -46,9 +50,10 @@ public class App {
                 } else if (outText.equals("yes")) {
                     continue;
                 }
-            } catch (ArithmeticException | IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
+//            } catch (Exception e) {
+//                System.out.println(e.getMessage());
+
+//            }
         }
     }
 }
